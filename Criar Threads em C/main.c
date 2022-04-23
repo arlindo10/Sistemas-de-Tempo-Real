ed23dc3af814425614c83c48081cffc3a179e4fc
Arlindo <arlindoalcantara3@gmail.com>
2022-04-22 22:33:55 -0300

    Código finalizado.

--- Criar Threads em C/main.c
+++ Criar Threads em C/main.c
@@ -1,71 +1,85 @@
 #include <stdio.h>
-#include <errno.h>
-#include <stddef.h>     /* _threadid variable */
-#include <pthread.h> 
-#include <stdlib.h>
+#include <pthread.h>
 
-char listachar[30];
-int indice = 0;
+/*
+
+- Criar 3 threads utilizando a biblioteca pthread.h
+- Sincronizar as threads para que executem ordenamente utilizando o busy wait loop e flag
+- As threads devem preencher o vetor nesta ordem: 
+   TA ->'A' no primeiro elemento
+   TB -> 'B' no segundo elemento
+   TC -> 'C' no terceiro elemento
+- O vetor tem 30 elementos
+*/
+
+
+
+char arr[30];
+int letterIndex = 0;
+
+void escreverNoArr(char letra){
+	arr[letterIndex] = letra;
+	letterIndex += 1;
+}
+
+/* Sincronizando threads utilizando  busy wait loop*/
+
+pthread_t TA,TB,TC;
 int flag1 = 1;
 int flag2 = 0;
 int flag3 = 0;
-int turn = 0;
 
-void *thread_code_a(void *threadno)
-{
-  while (indice <= 27){
-   	  while(flag1 == 0){}; 
-      listachar[indice] = 'a';
-      indice++;
-      flag2 = 1;
-      flag1 = 0;
-  }
-  return NULL;
-    
+void *thread_A(void *threadno){
+	while(letterIndex <= 27){
+		while (flag1 == 0);
+		escreverNoArr('A');
+		flag2 = 1;
+		flag1 = 0;
+	}
+	return NULL;
 }
 
-void *thread_code_b(void *threadno)
-{
-  while (indice <= 28){
-   	  while(flag2 == 0){}; 
-      listachar[indice] = 'b';
-      indice++;
-      flag3 = 1;
-      flag2 = 0;
-   }
-   return NULL;
-   
+void *thread_B(void *threadno){
+	while(letterIndex <= 28) {
+		while (flag2 == 0);
+		escreverNoArr('B');
+		flag3 = 1;
+		flag2 = 0;
+	}
+
+	return NULL;
 }
 
-void *thread_code_c(void *threadno)
-{
-   while (indice <= 29){
-   	  while(flag3 == 0){}; 
-      listachar[indice] = 'c';
-      indice++;
-      flag1 = 1;
-      flag3 = 0;
-   }
-   for (indice = 0; indice <= 29; indice++){
-	   printf("%c\n", listachar[indice]);}
-   return NULL;
-   
+void *thread_C(void *threadno){
+	while(letterIndex <= 29) {
+		while (flag3 == 0);
+		escreverNoArr('C');
+		flag1 = 1;
+		flag3 = 0;		
+	}
+
+	return NULL;
 }
 
-int main(void)
-{
- pthread_t TA;
- pthread_t TB;
- pthread_t TC;
- 
- pthread_create(&TA, NULL, thread_code_a, NULL);
- pthread_create(&TB, NULL, thread_code_b, NULL);
- pthread_create(&TC, NULL, thread_code_c, NULL);
+void exibirVetor() {
+	for(int i = 0; i < 30; i++){
+		printf("%c", arr[i]);
+	}
+}
+
+int main(void) {
+
+  /* Criar 3 threads utilizando a biblioteca pthread.h*/
   
- pthread_join(TA,NULL); 
- pthread_join(TB,NULL);
- pthread_join(TC,NULL);
- 
-
- return 0;
+	pthread_create(&TA, NULL, thread_A, NULL);
+	pthread_create(&TB, NULL, thread_B, NULL);
+	pthread_create(&TC, NULL, thread_C, NULL);
+	
+	pthread_join(TA, NULL);
+	pthread_join(TB, NULL);
+	pthread_join(TC, NULL);
+	
+	exibirVetor();
+	return 0;
+	
 }
